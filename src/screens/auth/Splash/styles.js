b6498bf541a9e63b8c03c816cbc37e8643c3a508
silversirp <silver.sirp@khk.ee>
2023-09-27 12:40:52 +0300

    splash ekraan

---
+++ src/screens/auth/Splash/styles.js
@@ -0,0 +1,16 @@
+import {Stylesheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: "bold"
+    },
+    innerTitle: {
+        color: '#FCA34D',
+        TextDecorationLine: 'underline'
+    }
+})
