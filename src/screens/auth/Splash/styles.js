324a6d1ce14f679611c3be58f4d20c79024d7a20
silversirp <silver.sirp@khk.ee>
2023-10-03 11:31:19 +0300

    splash screen

--- src/screens/auth/Splash/styles.js
+++ src/screens/auth/Splash/styles.js
@@ -1,16 +1,35 @@
-import {Stylesheet} from "react-native";
+import {StyleSheet} from "react-native";
 
 export const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        flexDirection: 'column',
+        justifycontent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        borderWidth: 1
+    },
     image: {
         width: '100%',
         height: 200
     },
+    titleContainer: {
+        marginVertical: 54
+    },
     title: {
         fontSize: 40,
-        fontWeight: "bold"
+        fontWeight: "bold",
+        textAlign: 'center'
     },
     innerTitle: {
         color: '#FCA34D',
-        TextDecorationLine: 'underline'
+        textDecorationLine: 'underline'
+    },
+    footerText: {
+        color: '#4F63AC',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 30
     }
 })
